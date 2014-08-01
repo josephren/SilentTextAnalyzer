b8b00c0c20d364f6e582c3d6bef2a19d8c6c75b5
josephren <renwangnow@gmail.com>
2014-08-02 00:43:03 +0800

    add hash function

--- rabinkarp.py
+++ rabinkarp.py
@@ -5,3 +5,12 @@ class RabinKarp:
         self.R = 256
         self.Q = 997
         self.pattern = pattern
+    def hash(self,word):
+        hash = 0
+        for i in range(len(word)):
+            hash = (hash * self.R + ord(word[i:i+1])) % self.Q
+        return hash
+
+if __name__ == "__main__":
+    rb = RabinKarp("hello")
+    print rb.hash("hello")
