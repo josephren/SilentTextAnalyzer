2d19846e917faab61aa1024f1f77f8922b764f04
josephren <renwangnow@gmail.com>
2014-08-02 09:35:37 +0800

    start to design a new class

--- rabinkarp.py
+++ rabinkarp.py
@@ -1,16 +1,2 @@
 __author__ = 'renwang'
 
-class RabinKarp:
-    def __init__(self,pattern):
-        self.R = 256
-        self.Q = 997
-        self.pattern = pattern
-    def hash(self,word):
-        hash = 0
-        for i in range(len(word)):
-            hash = (hash * self.R + ord(word[i:i+1])) % self.Q
-        return hash
-
-if __name__ == "__main__":
-    rb = RabinKarp("hello")
-    print rb.hash("hello")
