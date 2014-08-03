575608b41da6733dc83c08c37a6df375672652f1
renwang.rw <renwangnow@gmail.com>
2014-08-03 13:58:40 +0800

    add the count_freq utils

---
+++ application/count_freq.py
@@ -0,0 +1,9 @@
+from string.tries import Tries
+
+__author__ = 'renwang.rw'
+file_name = ""
+pattern_list = []
+pattern_dict = {}
+tries = Tries()
+for line in open(file_name):
+    result =
--- string/codeutils.py
+++ string/codeutils.py
@@ -5,3 +5,6 @@ def change_to_utf(in_name,out_name):
     for line in open(in_name):
         out_file.write(line.decode('gbk').encode('utf-8'))
     out_file.close()
+
+if __name__ == "__main__":
+    change_to_utf("d:/data/7_ivr.csv","d:/data/7_ivr_utf.csv")
