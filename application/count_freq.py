4e8e6ae79c4776f240c80bd94b24b2e72e58246c
josephren <renwangnow@gmail.com>
2014-08-03 14:11:22 +0800

    update regular and similarity

--- application/count_freq.py
+++ application/count_freq.py
@@ -1,9 +1,22 @@
+# coding=utf-8
 from string.tries import Tries
 
 __author__ = 'renwang.rw'
-file_name = ""
-pattern_list = []
+in_file_name = "testdata/application/freq"
+out_file_name = in_file_name + "_result"
+pattern_list = ["小猫", "小狗","喜欢"]
 pattern_dict = {}
 tries = Tries()
-for line in open(file_name):
-    result =
+tries.put_list(pattern_list)
+for token in pattern_list:
+    pattern_dict[token] = 0
+
+for line in open(in_file_name):
+    result = tries.search_line(line)
+    for token in result:
+        pattern_dict[token] += 1
+
+out_file_path = open(out_file_name, 'wb')
+for key in pattern_dict:
+    out_file_path.write(key + '\t' + str(pattern_dict[key]) + '\n')
+out_file_path.close()
