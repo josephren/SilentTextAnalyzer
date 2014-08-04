e6d75200041b07cff1ee70c93810e8fca563bf1d
josephren <renwangnow@gmail.com>
2014-08-04 16:44:34 +0800

    add 3d plot moduel

---
+++ plot_utils/3d.py
@@ -0,0 +1,9 @@
+from Carbon.Fonts import times
+
+__author__ = 'renwang'
+import matplotlib.pyplot as plt
+x = range(3,9)
+y = range(4,10)
+plt.plot(x,y,'ro')
+plt.show()
+times.sleep(10)
---
+++ plot_utils/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'renwang'
+import numpy
+print numpy.arange(9).reshape(3,3)
